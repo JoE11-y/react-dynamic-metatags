a35592f11f3e92c59fefa0e0aeb0842120a48bc4
JoE11-y <josephedoh77@gmail.com>
2023-07-02 19:15:52 +0100

    add twitter tags

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
-const { getPostById } = require("./stub/posts");
 const PORT = process.env.PORT || 3000;
 const base = process.env.BASE || '/'
 const axios = require("axios");
@@ -37,10 +36,13 @@ app.get("/*", (req, res, next) => {
       const image = `https://bafybeies3odi24wyk3e22rnautr57tiuk3b56nxrd53fxgtvr37abmz5j4.ipfs.dweb.link/${index}.png`;
       
       htmlData = htmlData
-      .replace("<title>Azuki|LooksRare</title>", `<title>${meta?.data.name} - Azuki|LooksRare</title>`)
+      .replace("<title>Azuki | LooksRare</title>", `<title>${meta?.data.name} - Azuki | LooksRare</title>`)
       .replace("__META_OG_TITLE__", `${meta?.data.name} - Azuki|LooksRare`)
-      .replace("__META_OG_DESCRIPTION__", `LocksRare is a Community-first Marketplace for NFT's and digital.`)
-      .replace("__META_OG_IMAGE__", image);
+      .replace("__META_OG_DESCRIPTION__", `LooksRare is a Community-first Marketplace for NFT's and digital.`)
+      .replace("__META_OG_IMAGE__", image)
+      .replace("__META_OGT_TITLE__", `${meta?.data.name} - Azuki|LooksRare`)
+      .replace("__META_OGT_DESCRIPTION__", `LooksRare is a Community-first Marketplace for NFT's and digital.`)
+      .replace("__META_OGT_IMAGE__", image);
     }
     return res.send(htmlData);
   });
